770d51d38619bbf7bfe7d6e5cea23d981f07f5f4
Danyl <danylo.antonenko@nure.ua>
2019-08-06 16:19:24 +0300

    First unfinished version

---
+++ Dictaphone/app/src/main/java/com/example/dictaphone/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.dictaphone;
+
+import android.content.Context;
+import android.graphics.drawable.Animatable2;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView smallest_line, middle_line, biggest_line;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smallest_line = findViewById(R.id.smallest_line);
+        middle_line = findViewById(R.id.middle_line);
+        biggest_line = findViewById(R.id.biggest_line);
+
+        smallest_line.setVisibility(View.INVISIBLE);
+        middle_line.setVisibility(View.INVISIBLE);
+        biggest_line.setVisibility(View.INVISIBLE);
+
+        registerForContextMenu(smallest_line);
+        registerForContextMenu(middle_line);
+        registerForContextMenu(biggest_line);
+
+
+            startAnimation();
+            stopAnimation();
+            startAnimation();
+            stopAnimation();
+
+            //HelloWorld
+        
+    }
+
+
+
+    public void startAnimation(){
+        if(smallest_line.getAnimation() == null){
+
+            try {
+                smallest_line.setAnimation(createAnimation(0));
+                middle_line.setAnimation(createAnimation(250));
+                biggest_line.setAnimation(createAnimation(500));
+
+            } catch (IllegalArgumentException ex){
+                Toast.makeText(this, ex.getMessage() + "\nError caused by calling startAnimation()", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    public void stopAnimation(){
+        if(smallest_line.getAnimation() != null){
+
+
+            smallest_line.setAnimation(null);
+            middle_line.setAnimation(null);
+            biggest_line.setAnimation(null);
+
+            smallest_line.clearAnimation();
+            middle_line.clearAnimation();
+            biggest_line.clearAnimation();
+
+            smallest_line.setVisibility(View.INVISIBLE);
+            middle_line.setVisibility(View.INVISIBLE);
+            biggest_line.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public Animation createAnimation(int delay) throws IllegalArgumentException{
+        if(delay >= 0) {
+            Animation animation = new AlphaAnimation(0, 1);
+            animation.setDuration(750);
+            animation.setRepeatMode(Animation.REVERSE);
+            animation.setRepeatCount(Animation.INFINITE);
+            animation.setStartTime(AnimationUtils.currentAnimationTimeMillis() + delay);
+            return animation;
+        }
+        else{
+            throw new IllegalArgumentException("Delay must not be less then 0.");
+        }
+    }
+
+}
